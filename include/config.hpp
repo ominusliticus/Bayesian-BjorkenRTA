256deb2e36b8f76f0bb7673390546dcd4c3c4b78
ominusliticus <elkevin111@gmail.com>
2022-04-27 16:43:21 -0400

    Amend to last commit

--- include/config.hpp
+++ include/config.hpp
@@ -1,15 +1,24 @@
 //
 // Author: Kevin Ingles
+// File: config.hpp
+// Description: For defining macros for different platforms and run configurations
 
-// File for defining all the preprocessing commands for all other files
-
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
 
 // Uncomment to enable parallel evaluation
 // Parallelizing used for ExactSolution.cpp and BayesianParameterEstimation.hpp
+#if __APPLE__
+// My apple machine currently only supports single core processing
+#define USE_PARALLEL 0
+#else
 #define USE_PARALLEL 1
+#endif
 
 // Need for dertain inversions of determinatn calculations
 #define ARMA_DONT_USE_WRAPPER
 #ifndef ARMA_USE_LAPACK
     #define ARMA_USE_LAPACK
+#endif
+
 #endif
