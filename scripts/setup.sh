93bcd92d307815e02237748611791bcf0ad022b8
ominusliticus <elkevin111@gmail.com>
2022-07-27 22:35:12 -0400

    Clean-up setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,16 +4,6 @@ echo "Checking for 3rd party libraries, and installing if needed"
 echo pwd
 cd ../3rd_party
 
-if [[ -d fmt ]]
-then
-    echo 'fmt library exists'
-else
-    echo "fmt repository doesn't exist"
-    echo "cloning repository https://github.com/fmtlib/fmt.git"
-    git clone --recursive https://github.com/fmtlib/fmt.git
-    echo "Done"
-fi
-
 if [[ -d aramadillo ]]
 then 
     echo "armadillo library exists"
@@ -29,19 +19,17 @@ cd ../
 echo "Done installing all dependencies."
 
 echo "Creating necessary directories"
-mkdir build
-mkdir output
-mkdir output/exact
-mkdir output/aniso_hydro
-mkdir output/bayes
-mkdir output/CE_hydro
-mkdir output/DNMR_hydro
-cd scripts/
-mkdir design_points
-mkdir full_outputs
-mkdir hydro_simulation_points
-mkdir pickle_files
-mkdir swap
+mkdir -p build
+mkdir -p output
+mkdir -p output/exact
+mkdir -p output/aniso_hydro
+mkdir -p output/bayes
+mkdir -p output/CE_hydro
+mkdir -p output/DNMR_hydro
+mkdir -p scripts/design_points
+mkdir -p scripts/full_outputs
+mkdir -p scripts/hydro_simulation_points
+mkdir -p scripts/pickle_files
+mkdir -p scripts/swap
 
-cd ../
 echo "Directories created"
