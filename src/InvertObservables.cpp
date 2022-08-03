1c4a5ea4d8e431a9ecc014ed9fe5ef1dd3957715
ominusliticus <elkevin111@gmail.com>
2022-08-03 18:02:14 -0400

    Invert observables: Translating more of Mike's code to my own

--- src/InvertObservables.cpp
+++ src/InvertObservables.cpp
@@ -38,6 +38,9 @@ using SP  = SimulationParameters;
 using vec = arma::vec;
 static hydro::AltAnisoHydroEvolution evo;
 
+constexpr double tol_dX = 1e-4;
+constexpr double tol_F	= 1e-4;
+
 /// hydro_fields is vector of the form (E, PT, PL)
 /// aniso_vars is vector of the form (alpha, Lambda, xi)
 vec&& ComputeF(const vec& hydro_fields, double mass, const vec& aniso_vars)
@@ -53,14 +56,58 @@ vec&& ComputeF(const vec& hydro_fields, double mass, const vec& aniso_vars)
 
 /// hydro_fields is vector of the form (E, PT, PL)
 /// aniso_vars is vector of the form (alpha, Lambda, xi)
-double LineBackTrack(const vec& hydro_fields, const double& aniso_vars, const double& delta_ansio_vars, double mass)
+/// Line backtracing algorithm taken from Numerical Recipes pgs. 478-489
+double LineBackTrack(const vec& hydro_fields, const double& aniso_vars, const double& delta_aniso_vars, double mass)
 {
-	vec	   aniso_vars_update = aniso_var;
+	vec	   aniso_vars_update = aniso_vars;
 	vec	   F				 = ComputeF(hydro_fields(0), hydro_fields(1), hydro_fields(2), mass, aniso_vars_update);
-	double mag_F			 = arma::norm(F, 2);
+	double mag_F			 = 0.5 * arma::norm(F, 2);
+	double mag_dX			 = arma::norm(delta_aniso_vars, 2);
+
+	double step_adj		 = 1.;			 ///< parameter returned by line brack-trace algo
+	double alpha		 = 1e-4;		 ///< Descent rate
+	double g0			 = mag_F;		 ///< g(x) is aux function to help us minimize search
+	double g0_prime		 = -2.0 * g0;	 ///< g'(x) evaluated at x_0
+	double step_adj_root = -g0_prime / (2.0 * (mag_F - g0 - g0_prime));	   // Starting guess
+	double step_adj_prev = step_adj_root;
+	double mag_F_current = mag_F;
+	double mag_F_prev	 = mag_F;
+	for (int i = 0; i < 20; ++i)
+	{
+		if (step_adj * mag_dX <= tol_dX) return step_adj;						// Check if converged
+		else if (mag_F <= g0 + step_adj * alpha * g0_prime) return step_adj;	// Check if converging fast enough
+		else
+		{
+			double a = (mag_F_current - step_adj * g0_prime) / (step_adj * step_adj);
+			a -= (mag_F_prev - g0 - step_adj_prev * g0_prime) / (step_adj_prev * step_adj_prev);
+			a /= (step_adj - step_adj_prev);
+			double b = step_adj_prev * (mag_F_current - step_adj * g0_prime) / (step_adj * step_adj);
+			b += step_adj * (mag_F_prev - g0 - step_adj_prev * g0_prime) / (step_adj_prev * step_adj_prev);
+			b /= (step_adj - step_adj_prev);
+
+			if (a == 0) step_adj_root = -g0_prime / (2.0 * b);	  // root if g(x) is quadratic
+			else
+			{
+				double z = b * b - 3.0 * a * g0_prime;
+				if (z < 0) step_adj_root = 0.5 * step_adj;
+				else if (b <= 0) step_adj_root = (-b + std::sqrt(z)) / (3.0 * a);
+				else step_adj_root = -g0_prime / (b + std::sqrt(z));
+			}
+			step_adj_root = std::fmin(step_adj_root, 0.5 * step_adj);
+		}
+		step_adj_prev	  = step_adj;
+		mag_F_prev		  = mag_F_current;
+		step_adj		  = std::fmax(step_adj_root, 0.1 * step_adj);
+		aniso_vars_update = aniso_vars + step_adj * delta_aniso_vars;
+		F				  = ComputeF(hydro_fields, mass, aniso_vars_update);
+		mag_F_current	  = 0.5 * arma::norm(F, 2);
+	}
+	return step_adj;
 }
 
 // ----------------------------------------
 
-double FindAnisoVariables();
+double FindAnisoVariables(double E, double PT, double PL, double mass)
+{
+}
 
