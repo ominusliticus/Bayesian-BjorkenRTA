9bf6e6790204dbd3035d2ed22cf9a57e84f402d5
ominusliticus <elkevin111@gmail.com>
2023-12-06 14:38:03 -0600

    Update test script to automatically generate the dir specificied in file

--- test_run.sh
+++ test_run.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -x
 
-mkdir -p output/maroon
+ouput_dir='output/test_no_filesystem'
+mkdir -p $ouput_dir
 make 
-./build/exact_solution.x tau_0 0.1 e0 12.4625 pt0 1.99149 pl0 8.18535 tau_f 12.1 mass 1.01523 C 0.39 $1 "output/test_exact"
+./build/exact_solution.x tau_0 0.1 e0 12.4625 pt0 1.99149 pl0 8.18535 tau_f 12.1 mass 1.01523 C 0.39 $1 $ouput_dir
